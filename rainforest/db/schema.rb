434a37db1643d6f096b841129e304225bd74a54e
Jon Greenspan <jgreenspan10@gmail.com>
2015-11-15 22:49:51 -0500

    Creating a user model, controller and view

--- rainforest/db/schema.rb
+++ rainforest/db/schema.rb
@@ -21,4 +21,11 @@ ActiveRecord::Schema.define(version: 20151114021036) do
     t.datetime "updated_at",     null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
 end
