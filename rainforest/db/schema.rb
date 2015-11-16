c3f457a20dea48f6c5b6b62578a17b960ad2f085
Jon Greenspan <jgreenspan10@gmail.com>
2015-11-15 21:25:27 -0500

    Generating the products controller and views

--- rainforest/db/schema.rb
+++ rainforest/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113194715) do
+ActiveRecord::Schema.define(version: 20151114021036) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
